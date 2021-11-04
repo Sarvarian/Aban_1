c8349989abadc70a202e41b6fc0f9e6ce7f43587
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-11-04 05:54:01 +0330

    add sdl2 libarry

--- src-c/os/os.c
+++ src-c/os/os.c
@@ -1,7 +1,13 @@
+#include <SDL.h>
+
 int os_init(const int argc, const char *argv[])
 {
+    SDL_Init(0);
     return 0;
 }
 
-void os_exit() {}
+void os_exit()
+{
+    SDL_Quit();
+}
 void os_error() {}
