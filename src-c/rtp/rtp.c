4d50b55e85ad1c7288501ac7c058d3767d973f6a
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-11-24 12:30:01 +0330

    Update rtp.c

--- src-c/rtp/rtp.c
+++ src-c/rtp/rtp.c
@@ -1,10 +1,23 @@
 #include "SDL_thread.h"
 
+static SDL_Thread *rtp_hThread = NULL;
+static char rtp_should_exit = 0;
+
 int rtp_init()
 {
+    static int rtp_main(void *);
+    rtp_hThread = SDL_CreateThread(rtp_main, "Real-Time-Process", NULL);
     return 0;
 }
 
 void rtp_exit()
 {
+    rtp_should_exit = 1;
+    int status = 0;
+    SDL_WaitThread(rtp_hThread, &status);
+}
+
+static int rtp_main(void *_data)
+{
+    return 0;
 }
