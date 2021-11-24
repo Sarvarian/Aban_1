3cd9e5cbdb6ef89c80ec9c68e0ec6d44c69b7e5e
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-11-24 06:04:06 +0330

    Update wm.c

--- src-c/wm/wm.c
+++ src-c/wm/wm.c
@@ -1,7 +1,8 @@
 #include "SDL_events.h"
+#include "SDL_log.h"
 #include "stdio.h"
-
-static SDL_Window *windows[8];
+#define MAX_WINDOWS 8
+static SDL_Window *windows[MAX_WINDOWS];
 
 int wm_init()
 {
@@ -10,6 +11,15 @@ int wm_init()
 
 void wm_exit()
 {
+    for (Uint8 i = 0; i < MAX_WINDOWS; i++)
+    {
+        SDL_WindowEvent *window = windows[i];
+        if (window != NULL)
+        {
+            SDL_DestroyWindow(window);
+            windows[i] = NULL;
+        }
+    }
 }
 
 void wm_close_window()
@@ -17,5 +27,25 @@ void wm_close_window()
 }
 
 void wm_window_event(SDL_WindowEvent *event)
+{
+    static process_window_event(SDL_Window * window, SDL_Event * event);
+
+    for (Uint8 i = 0; i < MAX_WINDOWS; i++)
+    {
+        const Uint32 id = SDL_GetWindowID(windows[0]);
+        if (id == 0)
+        {
+            SDL_LogError(SDL_LOG_PRIORITY_ERROR, "In processing an window event in Window Manager module, Failed to get Window ID. SDL Error: %s", SDL_GetError());
+        }
+
+        if (id == event->windowID)
+        {
+            process_window_event(windows[i], event);
+            return;
+        }
+    }
+}
+
+static process_window_event(SDL_Window *window, SDL_Event *event)
 {
 }
